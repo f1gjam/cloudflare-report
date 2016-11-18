f598c4f912342407c83f86c26b5485e90a878587
Kashif Ali <kashif@unixcraft.uk>
2016-11-18 10:31:56 +0000

    removed the cli thinking its not needed

--- cloudflare_report.py
+++ cloudflare_report.py
@@ -1,20 +1,18 @@
 from __future__ import absolute_import
 from __future__ import print_function
 
-import cloudflare_cli
 import cloudflare_filter_records
 import cloudflare_yaml_io
 
 
 def main():
-    cli_arg_params = cloudflare_cli.cli_args()
 
-
+    domain = 'metapack.com'
+    stack_list = ['dm', 'dm2', 'dm3',  'dm4',  'dm5',  'dm6', ]
     zone_record_dict = cloudflare_yaml_io.read_yaml_backup_file()
 
     filter_list_dict = cloudflare_filter_records.filter_stack(
-        zone_record_dict, cli_arg_params['domain'], cli_arg_params['stack'])
-
+        zone_record_dict, domain , stack_list)
 
     print(filter_list_dict)
 
